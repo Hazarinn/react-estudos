029b2bcdf0023616fc44d7fa5fd824dac4fdfef6
Rubens <devbsb.rubens@gmail.com>
2021-08-12 14:28:15 -0300

    estudos

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function App
-(){
-    return( <div>
-        <h1>Social midia</h1>
-    </div>
+const BemVindo = (props) =>{
+    return(
+
+        <div>
+            <h2>Bem vindo {props.nome}</h2>
+        </div>
     )
+
+} 
+
+function App(){
+    return(
+     <div>
+         ola mundo
+         <BemVindo nome ="Mateus"/>
+
+         <h1>curso react</h1>
+        
+    </div>
+    );
 }
 
 export default App;
